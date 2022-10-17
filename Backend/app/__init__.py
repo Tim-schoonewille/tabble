06a2ddd06dd86f2f9c4077079898b90ec7347be1
Tim Schoonewille <tim.schoonewille@gmail.com>
2022-10-17 12:06:20 -0500

    Add SQLALCHEMY extension to ext file and update app.config

--- Backend/app/__init__.py
+++ Backend/app/__init__.py
@@ -1,9 +1,17 @@
 from flask import Flask
+from app.ext import db
+
 
 
 def create_app():
 
     app = Flask(__name__)
-    
+
+    app.config.update(
+        SQLALCHEMY_DATABASE_URI = 'file:///db.sqlite3'
+        SQLALCHEMY_TRACK_MODIFICATIONS = False
+    )
+
+    db.init_app(app)
 
     return app
