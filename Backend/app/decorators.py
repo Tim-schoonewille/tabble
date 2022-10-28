53f14d25255821da7d513ce26405e239b521596a
Tim-Schoonewille <tim.schoonewille@gmail.com>
2022-10-28 10:38:59 +0200

    Remove admin decorator from init to decorator.py

---
+++ Backend/app/decorators.py
@@ -0,0 +1,21 @@
+from functools import wraps
+from flask_jwt_extended import verify_jwt_in_request
+from flask_jwt_extended import get_jwt
+from flask import jsonify
+
+
+
+def admin_required():
+    def wrapper(fn):
+        @wraps(fn)
+        def decorator(*args, **kwargs):
+            verify_jwt_in_request()
+            claims = get_jwt()
+            if claims["is_admin"]:
+                return fn(*args, **kwargs)
+            else:
+                return jsonify(content="Admins only!"), 403
+
+        return decorator
+
+    return wrapper
